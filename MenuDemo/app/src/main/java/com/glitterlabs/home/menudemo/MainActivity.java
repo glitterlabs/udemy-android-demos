d69f884580ae372c8e4e2c619d91e02be00b7bee
ashahu <ashahu@gmail.com>
2019-02-18 16:09:34 +0530

    adding Menudemos

---
+++ MenuDemo/app/src/main/java/com/glitterlabs/home/menudemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.glitterlabs.home.menudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+
+            case R.id.setting:
+                Log.i("Menu item Selected", "Setting");
+                return true;
+            case R.id.help:
+                Log.i("Menu item Selected", "help");
+                return true;
+                default:
+                    return false;
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
